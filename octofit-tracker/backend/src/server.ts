38876bbb3b4fba9f4ae9a3cf352f61b253685cd0
cet-mliu <mliu@cetaris.com>
2026-08-24 02:50:57 +0000

    step4-2

--- octofit-tracker/backend/src/server.ts
+++ octofit-tracker/backend/src/server.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import './config/database';
-import { PORT, API_BASE_URL } from './config/appConfig'; 
+//import { PORT, API_BASE_URL } from './config/appConfig'; 
 import usersRouter from './routes/users';
 import activitiesRouter from './routes/activities';
-
+const API_BASE_URL = process.env.CODESPACE_NAME
+  ? `https://${process.env.CODESPACE_NAME}-8000.app.github.dev`
+  : `http://localhost:${process.env.PORT}`;
 dotenv.config();
 
 const app = express();
@@ -20,7 +22,7 @@ app.get('/api/health', (_req: Request, res: Response) => {
 app.use('/api/users', usersRouter);
 app.use('/api/activities', activitiesRouter);
 
-app.listen(PORT, () => {
-  console.log(`Octofit Tracker API running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Octofit Tracker API running on port ${process.env.PORT}`);
   console.log(`API base URL: ${API_BASE_URL}`);
 });
